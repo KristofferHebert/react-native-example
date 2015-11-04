ebe182e2b811f47d159a3b35d7fd2c26d75d4950
= <kristoffer@kristofferhebert.com>
2015-11-03 19:17:32 -0800

    adding geolocation code

--- index.ios.js
+++ index.ios.js
@@ -5,37 +5,70 @@ import React from 'react-native'
 // https://facebook.github.io/react-native/docs/geolocation.html#content
 
 const Styles = React.StyleSheet.create({
-    text : {
-        color: 'black',
-        backgroundColor: 'white',
-        fontSize: 20,
-        margin: 80
-    }
+	text: {
+		color: 'black',
+		backgroundColor: 'white',
+		fontSize: 20,
+		margin: 80
+	}
 })
 
-class GeoLocate extends React.Component {
-    watchId: null,
-    getIntialState(){
-
-	// set initial cordinate values
-        return {
-            initialPostion: 'unknown',
-            lastPostion: 'unknown'
-        }
-    }
-    componentDidMount(){
-	// fetch data and update state
-    }
+const GeoLocateSettings = {
+	enableHighAccuracy: true,
+	timeout: 20000,
+	maximumAge: 1000
 }
 
-class reactNativeExampleApp extends React.Component {
-    render(){
-        return React.createElement(React.Text, {
-            style: Styles.text
-        }, "Hello World")
-    }
+let GeoLocate = React.createClass({
+	watchID: false,
+	getIntialState() {
+		// set initial cordinate values
+		return {
+			initialPostion: 'unknown',
+			lastPostion: 'unknown'
+		}
+	},
+	componentDidMount() {
+		// fetch data and update state
+		navigator.geolocation.getCurrentPosition(
+			(initialPosition) => this.setState({
+				initialPosition
+			}), (error) => alert(error.message),
+			GeoLocateSettings)
+		this.watchID = navigator.geolocation.watchPosition((lastPostion) => {
+			this.setState({
+				lastPostion
+			})
+		})
+	},
+	componentWillUnMount() {
+		navigator.geolocation.clearWatch(this.watchID)
+	},
+	render() {
+		return ( <View>
+			<Text>
+			<Text style = {
+				styles.title
+			}> Initial position: </Text> {
+				JSON.stringify(this.state.initialPosition)
+			}</Text><Text>
+			<Text style = {
+				styles.title
+			}> Current position: </Text> {
+				JSON.stringify(this.state.lastPosition)
+			}</Text> </View>
+		)
+	}
+})
+
+let reactNativeExampleApp = React.createClass({
+	render() {
+		return (
+            <GeoLocate />
+        )
+	}
 }
 
-React.AppRegistry.registerComponent('reactNativeExample', ()=>{
-    return reactNativeExampleApp
+React.AppRegistry.registerComponent('reactNativeExample', () => {
+	return reactNativeExampleApp
 })
